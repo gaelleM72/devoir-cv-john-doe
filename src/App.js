b017c70ce6676f7afcc95b5bb3b4eaed22a6ce83
gaelleM72 <141839974+gaelleM72@users.noreply.github.com>
2024-01-27 10:48:50 +0100

    préparation-projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+import { Navbar } from "./components/Navbar.jsx";
+import Blog from "./pages/Blog.jsx";
+import Realisations from "./pages/Realisations.jsx";
+import Contact from "./pages/Contact.jsx";
+import Services from "./pages/Services.jsx";
+import Accueil from "./pages/Accueil.jsx";
+import Mentions from "./pages/Mentions.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/realisations" element={<Realisations />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/mentions" element={<Mentions />} />
+        </Routes>
       </header>
+      <Footer />
     </div>
   );
 }
